f45dc65436ef4f3aa9afb0c596e367677c02eb6d
Mateo Matti <mateomatti06@gmail.com>
2025-12-05 12:51:10 -0500

    #8

---
+++ CheerfulChomps/Controllers/ReportsController.cs
@@ -0,0 +1,32 @@
+﻿using CheerfulChomps.Data;
+using CheerfulChomps.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace CheerfulChomps.Controllers
+{
+    [Authorize(Roles = "Administrator")]
+    public class ReportsController : Controller
+    {
+        // shared db connection
+        ApplicationDbContext _context;
+
+        // constructor w/db conn dependency
+        public ReportsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var products = _context.Product
+                .Include(r => r.Category)
+                .OrderBy(r => r.Name)
+                .ToList();
+
+            return View(Reports);
+        }
+
+    }
+}
